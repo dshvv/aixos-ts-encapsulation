be6f193e0b92662eee6f8dd3016aa2936d94a17e
丁少华 <dsh@192.168.0.100>
2020-01-14 00:04:58 +0800

    优化代码

--- src/http.ts
+++ src/http.ts
@@ -3,23 +3,23 @@ axios.defaults.baseURL = "http://dshvv.com:7001";
 axios.defaults.headers = {'Access-Control-Allow-Origin': '*'};
 
 class Http {
-    get(url: string, data?:any, headers?:any) {
+    protected get(url: string, data?:any, headers?:any) {
         return this.request('get', url, data, headers);
     }
 
-    post(url: string, data?:any, headers?:any) {
+    protected post(url: string, data?:any, headers?:any) {
         return this.request('post', url, data, headers);
     }
 
-    put(url: string, data?:any, headers?:any) {
+    protected put(url: string, data?:any, headers?:any) {
         return this.request('put', url, data, headers);
     }
 
-    delete(url: string, data?:any, headers?:any) {
+    protected delete(url: string, data?:any, headers?:any) {
         return this.request('delete', url, data, headers);
     }
 
-    request(method: Method, url: string, data:any, headers:any): AxiosPromise<any> {
+    protected request(method: Method, url: string, data:any, headers:any): AxiosPromise<any> {
         //公共头设置
         let aixosCtx: AxiosRequestConfig = { method, url };
         data && (aixosCtx.data = data);
